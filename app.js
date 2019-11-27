64e078e07f4e48469451c50198d767321a0847bf
Oceane <sautronoceane98@gmail.com>
2019-11-27 10:20:44 +0100

    chat fonctionne

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 var express = require('express');
-var mongoose = require('mongoose');
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
+var bodyParser = require('body-parser')
 var app = express();
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+
 
 //Shemas
 var Message = mongoose.model('Message',{
     name : String,
     message : String
   })
+
 //Url bdd
 mongoose.connect("mongodb+srv://oceane08:password974@cluster0-owldh.mongodb.net/test?retryWrites=true&w=majority", 
 {useNewUrlParser: true},() =>
 
 console.log("BDD CONNECTER")
 );
+
 
 app.use(express.static(__dirname));
 var bodyParser = require('body-parser')
@@ -24,28 +29,29 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 //middleware 
  
-//Route pour afficher les messages
 app.get('/messages', (req, res) => {
-    Message.find({},(err, messages)=> {
-      res.send(messages);
-    })
+  Message.find({},(err, messages)=> {
+    res.send(messages);
   })
+})
 
-//Route pour afficher les messages
-
-  app.post('/messages', (req, res) => {
-    var message = new Message(req.body);
-    message.save((err) =>{
-      if(err)
-        sendStatus(500);
-      res.sendStatus(200);
-    })
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
   })
+})
 
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+})
 
-
-
-
+  
 
 
 
